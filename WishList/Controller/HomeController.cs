04e18dd18860d2d7ad1f3f2d20a0711f1ee63964
Rabold, Frank <frank.rabold@klingelnberg.com>
2019-08-09 16:23:19 +0200

    Create Home Views and the HomeController

---
+++ WishList/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
